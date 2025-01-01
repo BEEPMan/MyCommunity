33d441ee3d510830af907cb0b82206f10323d431
현광빈 <bio8641@naver.com>
2025-01-02 03:40:27 +0900

    remove .git file in backend

---
+++ backend/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const frontendUrl = configService.get<string>('FRONTEND_URL');
+
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: frontendUrl,
+    credentials: true,
+  });
+
+  const port = configService.get<number>('PORT') || 3001;
+  await app.listen(port);
+}
+bootstrap();
